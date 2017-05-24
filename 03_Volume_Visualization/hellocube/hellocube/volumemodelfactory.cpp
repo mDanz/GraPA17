803b64e331f80ed98e343eb90b6d1729f2141bb7
Marcel Danz <marcel_danz@gmx.de>
2017-05-24 10:30:25 +0200

    refactoring

--- 03_Volume_Visualization/hellocube/hellocube/volumemodelfactory.cpp
+++ 03_Volume_Visualization/hellocube/hellocube/volumemodelfactory.cpp
@@ -16,85 +16,76 @@ void VolumeModelFactory::fillVolumeModel(const QString& fileName, VolumeModel &m
 {
 	QFile file(fileName);
 	if (!file.open(QIODevice::ReadOnly)) { return; }
-		auto bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto xDim = atoi(bytes);
-		delete[] bytes;
 
-		auto byte = new char;
-		file.read(byte, 1);
-		delete byte;
+	fillDimensions(model, file);
+	fillAspects(model, file);
+	fillTexture(model, file);
+}
 
-		bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto yDim = atoi(bytes);
-		delete[] bytes;
+void VolumeModelFactory::skipByte(QFile &file)
+{
+	auto byte = new char;
+	file.read(byte, 1);
+	delete byte;
+}
 
-		byte = new char;
-		file.read(byte, 1);
-		delete byte;
+int VolumeModelFactory::readInt(QFile &file)
+{
+	auto bytes = new char[4];
+	file.read(bytes, 3);
+	bytes[3] = '\0';
+	auto i = atoi(bytes);
+	delete[] bytes;
+	return i;
+}
 
-		bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto zDim = atoi(bytes);
-		delete[] bytes;
+float VolumeModelFactory::readFloat(QFile &file)
+{
+	auto bytes = new char[4];
+	file.read(bytes, 3);
+	bytes[3] = '\0';
+	auto f = atof(bytes);
+	delete[] bytes;
+	return f;
+}
 
-		byte = new char;
-		file.read(byte, 1);
-		delete byte;
+void VolumeModelFactory::fillDimensions(VolumeModel &model, QFile &file)
+{
+	auto xDim = readInt(file);
+	skipByte(file);
+	auto yDim = readInt(file);
+	skipByte(file);
+	auto zDim = readInt(file);
+	skipByte(file);
+	model.setDimensions(xDim, yDim, zDim);
+}
 
-		model.setDimensions(xDim, yDim, zDim);
+void VolumeModelFactory::fillAspects(VolumeModel &model, QFile &file)
+{
+	auto xAspect = readFloat(file);
+	skipByte(file);
+	auto yAspect = readFloat(file);
+	skipByte(file);
+	auto zAspect = readFloat(file);
+	skipByte(file);
+	model.setAspects(xAspect, yAspect, zAspect);
+}
 
-		bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto xAspect = atof(bytes);
-		delete[] bytes;
+void VolumeModelFactory::fillTexture(VolumeModel &model, QFile &file)
+{
+	int headerSize = 24;
+	int dataSize = file.size() - headerSize;
+	auto data = new char[dataSize];
+	file.read(data, dataSize);
 
-		byte = new char;
-		file.read(byte, 1);
-		delete byte;
+	unsigned char* byteData;
+	byteData = reinterpret_cast<unsigned char*>(data);
+	data = nullptr;
 
-		bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto yAspect = atof(bytes);
-		delete[] bytes;
-
-		byte = new char;
-		file.read(byte, 1);
-		delete byte;
-
-		bytes = new char[4];
-		file.read(bytes, 3);
-		bytes[3] = '\0';
-		auto zAspect = atof(bytes);
-		delete[] bytes;
-
-		byte = new char;
-		file.read(byte, 1);
-		delete byte;
-
-		model.setAspects(xAspect, yAspect, zAspect);
+	auto texture = new QOpenGLTexture(QOpenGLTexture::Target3D);
+	texture->setData(QOpenGLTexture::PixelFormat::Alpha, QOpenGLTexture::PixelType::UInt8, byteData);
+	texture->allocateStorage();
 		
-		int headerSize = 24;
-
-		int dataSize = file.size() - headerSize;
-		char* data = new char[dataSize];
-		file.read(data, dataSize);
-
-		unsigned char* byteData;
-		byteData = reinterpret_cast<unsigned char*>(data);
-		data = nullptr;
-
-		auto texture = new QOpenGLTexture(QOpenGLTexture::Target3D);
-		texture->setData(QOpenGLTexture::PixelFormat::Alpha, QOpenGLTexture::PixelType::UInt8, byteData);
-		texture->allocateStorage();
-
-		
-		delete[] byteData;
-		model.setTexture(texture);
+	delete[] byteData;
+	model.setTexture(texture);
 }
